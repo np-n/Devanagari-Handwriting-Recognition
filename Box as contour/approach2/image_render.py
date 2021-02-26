42559fe6e075e5f5d9a5022ec58e844422463512
nimesh44 <39429615+nimesh44@users.noreply.github.com>
2021-02-26 22:48:25 +0545

    Image name shortened
    time.time() is replaced by a unique variable for each image

--- Box as contour/approach2/image_render.py
+++ Box as contour/approach2/image_render.py
@@ -78,7 +78,7 @@ for image_identifier in range(1,scanned_input_image_count+1):
             gray_image, 180, 255, cv2.THRESH_BINARY_INV)
         kernel = np.ones((3,3), np.uint8) 
         dilated_image= cv2.dilate(thresh_image, kernel, iterations=1)
-        cv2.imwrite(os.path.join(output_dir,character_image_name+'_'+str(time.time())+str(i)+'.jpg'),dilated_image)
+        cv2.imwrite(os.path.join(output_dir,character_image_name+'_'+str(image_identifier)+str(i)+'.jpg'),dilated_image)
 
 if identifier == 1:
     print("You don't have any images now..")
